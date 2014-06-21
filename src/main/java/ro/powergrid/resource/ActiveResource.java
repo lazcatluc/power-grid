5eec2a24fb82c03d0a381e62e817ecea6d982cb8
lazcatluc <lazcatluc@gmail.com>
2014-06-21 13:12:11 +0300

    added display for current resource held

--- src/main/java/ro/powergrid/resource/ActiveResource.java
+++ src/main/java/ro/powergrid/resource/ActiveResource.java
@@ -1,23 +1,49 @@
 package ro.powergrid.resource;
 
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import java.util.Map;
+import javax.faces.bean.SessionScoped;
 
 /**
  * Created by adi on 6/21/14.
  */
 @ManagedBean(name = "activeResource", eager = true)
-public class ActiveResource {
-    public String updatePowerPlantResources(){
-        FacesContext fc = FacesContext.getCurrentInstance();
-
-        Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
-        String valueAsString = params.get("availableResources");
-        int resourceValue = Integer.parseInt(valueAsString);
+@SessionScoped
+public class ActiveResource implements Serializable {
+    private static final long serialVersionUID = 1l;
+    private int availableResources;
+    private Resource resource = Resource.NULL;
+    
+    public void updatePowerPlantResources() {
         ResourceType resourceType = new ResourceType("Oil");
-        Resource resource = new Resource(resourceValue, resourceType);
+        setResource(new Resource(availableResources, resourceType));
+    }
 
-        return "index";
+    /**
+     * @return the availableResources
+     */
+    public int getAvailableResources() {
+        return availableResources;
+    }
+
+    /**
+     * @param availableResources the availableResources to set
+     */
+    public void setAvailableResources(int availableResources) {
+        this.availableResources = availableResources;
+    }
+
+    /**
+     * @return the resource
+     */
+    public Resource getResource() {
+        return resource;
+    }
+
+    /**
+     * @param resource the resource to set
+     */
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 }
