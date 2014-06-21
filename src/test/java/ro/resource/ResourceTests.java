08f6bdabad219845eeafd1a6dc2ff11fe1e96ac8
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-06-21 17:10:44 +0300

    Deleted noisy comments

--- src/test/java/ro/resource/ResourceTests.java
+++ src/test/java/ro/resource/ResourceTests.java
@@ -8,8 +8,8 @@ import ro.powergrid.resource.ResourceType;
 public class ResourceTests {
     @Test
     public void whenDisplayedItShowsItsState(){
-        Resource resource = new Resource(13, new ResourceType("Oil"));
+        Resource resource = new Resource(13, ResourceType.OIL);
 
-        Assert.assertEquals("13 Oil", resource.toString());
+        Assert.assertEquals("13 OIL", resource.toString());
     }
 }
