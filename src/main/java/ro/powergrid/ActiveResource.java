b284b98b2e553508a368f9c2568a5295282bed68
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-06-21 12:34:30 +0300

    The power plant active resources can be used

--- src/main/java/ro/powergrid/ActiveResource.java
+++ src/main/java/ro/powergrid/ActiveResource.java
@@ -3,6 +3,8 @@ package ro.powergrid;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import java.util.Map;
 
 /**
  * Created by adi on 6/21/14.
@@ -11,12 +13,13 @@ import javax.faces.bean.RequestScoped;
 public class ActiveResource {
     public String updatePowerPlantResources(){
         FacesContext fc = FacesContext.getCurrentInstance();
+
         Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
         String valueAsString = params.get("availableResources");
         int resourceValue = Integer.parseInt(valueAsString);
         ResourceType resourceType = new ResourceType("Oil");
         Resource resource = new Resource(resourceValue, resourceType);
 
-        return "player-board";
+        return "index";
     }
 }
