9b3d11dcd892f345b65c046f48d16118211ea3f2
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-06-21 12:04:26 +0300

    Power plant resources not finalized yet

---
+++ src/main/java/ActiveResource.java
@@ -0,0 +1,22 @@
+package ro.powergrid;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * Created by adi on 6/21/14.
+ */
+@ManagedBean(name = "activeResource", eager = true)
+public class ActiveResource {
+    public String updatePowerPlantResources(){
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+        String valueAsString = params.get("availableResources");
+        int resourceValue = Integer.parseInt(valueAsString);
+        ResourceType resourceType = new ResourceType("Oil");
+        Resource resource = new Resource(resourceValue, resourceType);
+
+        return "player-board";
+    }
+}
---
+++ src/main/java/Resource.java
@@ -0,0 +1,16 @@
+package ro.powergrid;
+/**
+ * Created by adi on 6/21/14.
+ */
+public class Resource {
+    private int resourceValue;
+    private ResourceType resourceType;
+
+    public Resource(int resourceValue, ResourceType resourceType){
+        this.resourceValue= resourceValue;
+        this.resourceType = resourceType;
+    }
+
+    public int getValue() { return resourceValue; }
+    public int getResourceType() { return resourceType;}
+}
