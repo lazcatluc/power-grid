c2eab3a8117591a04d1fed9dab68f6c2fe1c0a6e
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-06-22 09:27:23 +0300

    Added ResourceTypes to show the types of resources into a drop down list

--- src/main/java/ro/powergrid/resource/ResourceTypes.java
+++ src/main/java/ro/powergrid/resource/ResourceTypes.java
@@ -1,19 +1,34 @@
 package ro.powergrid.resource;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 import java.util.*;
 
-public class ResourceTypes {
+@ManagedBean(name = "resources", eager = true)
+@SessionScoped
+public class ResourceTypes implements Serializable {
+
+    private ResourceType chosenResourceType;
 
     public List<ResourceType> list() {
         return getDefaultResourceTypes();
     }
 
-    private List<ResourceType> getDefaultResourceTypes() {
+    public List<ResourceType> getDefaultResourceTypes() {
         ResourceType[] allValues = ResourceType.values();
         List<ResourceType> defaultValues = new ArrayList<>(Arrays.asList(allValues));
         defaultValues.remove(ResourceType.NONE);
         Collections.sort(defaultValues, new ResourceTypeComparator());
         return defaultValues;
+    }
+
+    public ResourceType getChosenResourceType() {
+        return chosenResourceType;
+    }
+
+    public void setChosenResourceType(ResourceType chosenResourceType) {
+        this.chosenResourceType = chosenResourceType;
     }
 
     static class ResourceTypeComparator implements Comparator<ResourceType> {
