f1bf89759c53c6a67b7abea6881939df6cb78952
lazcatluc <lazcatluc@gmail.com>
2014-07-31 14:21:43 +0300

    refactored connection test

--- src/test/java/ro/powergrid/city/ConnectionTest.java
+++ src/test/java/ro/powergrid/city/ConnectionTest.java
@@ -8,7 +8,19 @@ public class ConnectionTest {
 
 	@Test
 	public void directConnectionsAreAdditive() throws Exception {
-		assertEquals(2, new Connection(new DirectConnection(1), 
-				new DirectConnection(1)).getDistance());
+		assertEquals(2, onePlusOne().getDistance());
+	}
+
+	@Test
+	public void connectionOf2IsGreaterThanConnectionOf1() throws Exception {
+		assertTrue(onePlusOne().compareTo(one()) > 0);
+	}
+
+	public Connection onePlusOne() {
+		return new Connection(one(), one());
+	}
+
+	public DirectConnection one() {
+		return new DirectConnection(1);
 	}
 }
